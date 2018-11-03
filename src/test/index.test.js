89f0641cca921d60fa29c62b2904298fb79d7785
Amal S Raj <amalsraj@cet.ac.in>
2018-11-03 13:45:30 +0530

    add one comment

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,14 +1,17 @@
 let codeEvaluator = require('../index');
+
+//can be used in an express get() or post() functions.
 async function TestFunction(){
   let evalInstance = {
     language: 'testlanguage',
     input: 'jessal',
     code: 'name = raw_input()\nprint(name)'
   };
+
   let evaluator = codeEvaluator(evalInstance);
-  evaluator.evaluateCode(1).then(() => {
-    console.log(evaluator.resultSet)
-  });
+  await evaluator.evaluateCode(1)
+  console.log(evaluator.resultSet)
+  
 }
 
 TestFunction();

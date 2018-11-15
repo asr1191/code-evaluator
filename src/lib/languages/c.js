b189e6bf392388e7fd6679966161f16a2142ed47
Amal S Raj <amalsraj@cet.ac.in>
2018-11-15 12:51:46 -0800

    [feature] Add support for C and CPP

--- src/lib/languages/c.js
+++ src/lib/languages/c.js
@@ -8,32 +8,36 @@ function c(fileName, codeDir, inputDir, compileDir) {
     inputFileLocation: path.resolve(inputDir, `${fileName}.input`),
     compileFileLocation: path.resolve(compileDir, `${fileName}.out`),
 
-    runCode: async function runCode() {
-      function handleExecOutput(err, stdout, stderr) {
-        if (err && err.code !== 1) {
-          throw err;
-        } else {
-          return {
-            stdout,
-            stderr,
-          };
+    runCode: function runCode() {
+      return new Promise((resolve, reject) => {
+        function handleExecOutput(err, stdout, stderr) {
+          if (err && err.code !== 1) {
+            reject(err);
+          } else {
+            resolve({
+              stdout,
+              stderr,
+            });
+          }
         }
-      }
-      childProcess.exec(`${this.compileFileLocation} < ${this.inputFileLocation}`, { timeout: 10000 }, handleExecOutput);
+        childProcess.exec(`${this.compileFileLocation} < ${this.inputFileLocation}`, { timeout: 10000 }, handleExecOutput);
+      });
     },
 
-    compileCode: async function compileCode() {
-      function handleExecOutput(err, stdout, stderr) {
-        if (err) {
-          throw err;
-        } else {
-          return {
-            stdout,
-            stderr,
-          };
+    compileCode: function compileCode() {
+      return new Promise((resolve, reject) => {
+        function handleExecOutput(err, stdout, stderr) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve({
+              stdout,
+              stderr,
+            });
+          }
         }
-      }
-      childProcess.exec(`gcc ${this.codeFileLocation} -o ${this.compileFileLocation}`, { timeout: 10000 }, handleExecOutput);
+        childProcess.exec(`gcc -x c ${this.codeFileLocation} -o ${this.compileFileLocation}`, { timeout: 10000 }, handleExecOutput);
+      });
     },
   };
   return languageEvaluatorObject;
--- src/lib/languages/cpp.js
+++ src/lib/languages/cpp.js
@@ -1,42 +1,46 @@
 const childProcess = require('child_process');
 const path = require('path');
 
-function c(fileName, codeDir, inputDir, compileDir) {
+function cpp(fileName, codeDir, inputDir, compileDir) {
   const languageEvaluatorObject = {
     isCompilable: true,
     codeFileLocation: path.resolve(codeDir, fileName),
     inputFileLocation: path.resolve(inputDir, `${fileName}.input`),
     compileFileLocation: path.resolve(compileDir, `${fileName}.out`),
 
-    runCode: async function runCode() {
-      function handleExecOutput(err, stdout, stderr) {
-        if (err && err.code !== 1) {
-          throw err;
-        } else {
-          return {
-            stdout,
-            stderr,
-          };
+    runCode: function runCode() {
+      return new Promise((resolve, reject) => {
+        function handleExecOutput(err, stdout, stderr) {
+          if (err && err.code !== 1) {
+            reject(err);
+          } else {
+            resolve({
+              stdout,
+              stderr,
+            });
+          }
         }
-      }
-      childProcess.exec(`${this.compileFileLocation} < ${this.inputFileLocation}`, { timeout: 10000 }, handleExecOutput);
+        childProcess.exec(`${this.compileFileLocation} < ${this.inputFileLocation}`, { timeout: 10000 }, handleExecOutput);
+      });
     },
 
-    compileCode: async function compileCode() {
-      function handleExecOutput(err, stdout, stderr) {
-        if (err) {
-          throw err;
-        } else {
-          return {
-            stdout,
-            stderr,
-          };
+    compileCode: function compileCode() {
+      return new Promise((resolve, reject) => {
+        function handleExecOutput(err, stdout, stderr) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve({
+              stdout,
+              stderr,
+            });
+          }
         }
-      }
-      childProcess.exec(`g++ ${this.codeFileLocation} -o ${this.compileFileLocation}`, { timeout: 10000 }, handleExecOutput);
+        childProcess.exec(`g++ -x c++ ${this.codeFileLocation} -o ${this.compileFileLocation}`, { timeout: 10000 }, handleExecOutput);
+      });
     },
   };
   return languageEvaluatorObject;
 }
 
-module.exports = c;
+module.exports = cpp;

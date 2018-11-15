ab30ddeaf4693014b962adc43d72bfe18ad02668
Amal S Raj <amalsraj@cet.ac.in>
2018-11-15 12:49:43 -0800

    [bug-fix]  Revert to promises because code separation

--- src/lib/languages/python3.js
+++ src/lib/languages/python3.js
@@ -7,18 +7,20 @@ function python3(fileName, codeDir, inputDir) {
     codeFileLocation: path.resolve(codeDir, fileName),
     inputFileLocation: path.resolve(inputDir, `${fileName}.input`),
 
-    runCode: async function runCode() {
-      function handleExecOutput(err, stdout, stderr) {
-        if (err && err.code !== 1) {
-          throw err;
-        } else {
-          return {
-            stdout,
-            stderr,
-          };
+    runCode: function runCode() {
+      return new Promise((resolve, reject) => {
+        function handleExecOutput(err, stdout, stderr) {
+          if (err && err.code !== 1) {
+            reject(err);
+          } else {
+            resolve({
+              stdout,
+              stderr,
+            });
+          }
         }
-      }
-      childProcess.exec(`python3 ${this.codeFileLocation} < ${this.inputFileLocation}`, { timeout: 10000 }, handleExecOutput);
+        childProcess.exec(`python3 ${this.codeFileLocation} < ${this.inputFileLocation}`, { timeout: 10000 }, handleExecOutput);
+      });
     },
   };
   return languageEvaluatorObject;

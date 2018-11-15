4aa1dc76f6be5e3be0cf57c1f93ec07607fbb5e3
Amal S Raj <amalsraj@cet.ac.in>
2018-11-15 12:45:58 -0800

    [cleanup] Remove Java from imports

--- src/lib/languages/index.js
+++ src/lib/languages/index.js
@@ -1,6 +1,5 @@
 const c = require('./c');
 const cpp = require('./cpp');
-const java = require('./java');
 const python2 = require('./python2');
 const python3 = require('./python3');
 const testlanguage = require('./testlanguage');
@@ -8,7 +7,6 @@ const testlanguage = require('./testlanguage');
 module.exports = {
   c,
   cpp,
-  java,
   python2,
   python3,
   testlanguage,

c13af04775002b5b2758a3fdb59a78f0b38cc4f6
Amal S Raj <amalsraj@cet.ac.in>
2018-11-17 00:30:42 -0800

    [cleanup] More examples, error messages, compileCode return handling

--- examples/express-example.js
+++ examples/express-example.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const codeEvaluator = require('../src/index');
 
 // Paths to store code and inputs, which will later be run using the
@@ -8,26 +9,36 @@ const compileDir = '../evalInstanceFiles/binaries/';
 
 // can be used in an Express get() or post() functions.
 async function expressPOST() {
-  const evalInstance = {
-    // testlanguage is an example language, which returns a sample STDOUT and
-    // STDIN message, after a delay of 500ms.
+  // testlanguage is an example language, which returns a sample STDOUT and
+  // STDIN message, after a delay of 2000ms.
+  const evalInstancePython2 = {
+    language: 'python2',
+    input: 'jessal kid',
+    code: 'name = raw_input().split(" ")\nprint(name[0] + " is a good " + name[1])',
+  };
+
+  const evalInstanceCpp = {
     language: 'cpp',
-    // input: 'jessal kid',
-    // code: 'name = raw_input().split(" ")\nprint(name[0] + " is a good " + name[1])',
     input: 'Compilers',
     code: '#include<iostream>\n'
         + 'using namespace std;\n'
         + 'int main() {\n'
         + '   char name[10];'
         + '   cin >> name;\n'
-        + '   cout << "Hello " << name < "!";\n'
+        + '   cout << "Hello " << name << "!";\n'
         + ' return 0;\n'
         + '}',
+  };
+
+  const evalInstanceCppSegmentation = {
+    language: 'cpp',
+    input: '',
+    code: 'int main() { char *str;str = "GfG";*(str+1) = \'n\'; return 0; } ',
   };
   // Calling CodeEvaluator constructor function with an EvalInstance as
   // arguement to return a CodeEvaluator object, along with paths to store
   // the user's code and inputs that are to be run against the code.
-  const evaluator = codeEvaluator(evalInstance, codeDir, inputDir, compileDir);
+  const evaluator = codeEvaluator(evalInstanceCpp, codeDir, inputDir, compileDir);
 
   // passing an ID to the evaluator object, so that each compile request
   // can be referred to using its ID. Can be a number or string.
@@ -35,8 +46,9 @@ async function expressPOST() {
   try {
     await evaluator.saveCode(id);
     await evaluator.saveInput(id);
-    await evaluator.compileCode();
-    await evaluator.runCode();
+    if (await evaluator.compileCode()) {
+      await evaluator.runCode();
+    }
     // await evaluator.runCode()
     // If evaluation should happen with existing code and input files, the
     // runCode() method can be called instead of saveCode()
@@ -53,19 +65,19 @@ async function expressPOST() {
   //  3. Compilers/interpreters not installed (Error Code: COMPINT_UNAVAILABLE)
   } catch (e) {
     if (e.code === 'CODEDIR_NOEXIST') {
-      console.error(e.stack);
+      console.error(e.message);
     } else if (e.code === 'CODEFILE_NOWRITE') {
-      console.error(e.stack);
+      console.error(e.message);
     } else if (e.code === 'INPUTDIR_NOEXIST') {
-      console.error(e.stack);
+      console.error(e.message);
     } else if (e.code === 'INPUTFILE_NOWRITE') {
-      console.error(e.stack);
+      console.error(e.message);
     } else if (e.code === 'COMPILATION_ERROR') {
-      console.error(e.stack);
+      console.error(e.message);
     } else if (e.code === 'CODERUN_ERROR') {
-      console.error(e.stack);
+      console.error(e.message);
     } else {
-      console.error(e.stack);
+      console.error(e.message);
     }
   } finally {
     await evaluator.clearFiles();

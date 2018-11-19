3cf265b724db276b88cf8c8a24950aa4b1bd70df
Amal S Raj <amalsraj@cet.ac.in>
2018-11-20 00:14:40 +0530

    [test] Add init unit test for save-code.js

---
+++ src/lib/test/save-code.test.js
@@ -0,0 +1,10 @@
+/* global describe it */
+const chai = require('chai');
+
+const { expect } = chai;
+
+describe('MeTa TEsT LoL', () => {
+  it('LOl Wat am I doin?', () => {
+    expect('boom').to.equal('boom');
+  });
+});
